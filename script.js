c808b2248e63d127e7e689065c84446710b4f878
Priyam8878 <112754436+Priyam8878@users.noreply.github.com>
2023-06-21 15:48:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,26 +1,32 @@
-// function volume_sphere() {
-//     //Write your code here
-// 	const radius = document.getElementById("radius").value;
+function volume_sphere() {
+    //Write your code here
+	const radius = document.getElementById("radius").value;
 	
-//     const volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
+    const volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
 	
-// 	const VolumeInput = document.getElementById("volume")
-// 	VolumeInput.value =volume.toFixed(4);
-// }
+	const VolumeInput = document.getElementById("volume")
+	VolumeInput.value =volume.toFixed(4);
+}
   
  
 
-// window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
 
-function volume_sphere(event) {
-event.preventDefault(); // Prevent form submission
 
-let radius = document.getElementById("radius").value;
-let volume = (4 / 3) Math.PI Math.pow(radius, 3);
 
-document.getElementById("volume").value = volume.toFixed(4); // Adjust precision here
-}
 
-window.onload = function() {
-document.getElementById('MyForm').addEventListener('submit', volume_sphere);
-};
+
+
+
+// function volume_sphere(event) {
+// event.preventDefault(); // Prevent form submission
+
+// let radius = document.getElementById("radius").value;
+// let volume = (4 / 3) Math.PI Math.pow(radius, 3);
+
+// document.getElementById("volume").value = volume.toFixed(4); // Adjust precision here
+// }
+
+// window.onload = function() {
+// document.getElementById('MyForm').addEventListener('submit', volume_sphere);
+// }
